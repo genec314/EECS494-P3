3bc5484f7a2009c774bc1250c55f25947f326c46
genemc <genemc@umich.edu>
2022-03-12 17:46:32 -0500

    Add scene reset, lane boundaries, and physics materials

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameControl instance;
+    float first_thrown_time;
+    bool ball_thrown;
+    Subscription<BallThrownEvent> thrown_subscription;
+
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        thrown_subscription = EventBus.Subscribe<BallThrownEvent>(OnBallThrown);
+    }
+
+    void OnDestroy()
+    {
+        if (thrown_subscription != null) EventBus.Unsubscribe<BallThrownEvent>(thrown_subscription);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ball_thrown && first_thrown_time + 5f < Time.time)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
+    void OnBallThrown(BallThrownEvent e)
+    {
+        ball_thrown = true;
+        first_thrown_time = Time.time;
     }
 }
