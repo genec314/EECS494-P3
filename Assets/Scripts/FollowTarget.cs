c757c3d6fd9e4f715cc0e716c13fa3cff99171e6
shieldsb <shieldsb@umich.edu>
2022-03-11 20:31:54 -0500

    Roll Ball Lab

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+
+	public Transform target;
+	public Vector3 offset;
+	public float ease_factor = 0.1f;
+
+	bool following = false;
+	Subscription<BallThrownEvent> ball_thrown_sub;
+	// Use this for initialization
+	void Start()
+	{
+		ball_thrown_sub = EventBus.Subscribe<BallThrownEvent>(OnBallThrown);
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		if(following)
+			transform.position = Vector3.Lerp(transform.position, target.position + offset, ease_factor);
+	}
+
+	void OnBallThrown(BallThrownEvent e)
+    {
+		following = true;
+    }
+
+
+}
